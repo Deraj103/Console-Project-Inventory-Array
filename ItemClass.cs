80263707d2b981891efa612fd116c6433fdb85d3
JARED TIMS <jstims9109@gmail.com>
2024-02-20 20:24:31 -0700

    Add project files.

---
+++ ItemClass.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Console_Project_Inventory_Array
+{
+    class ItemClass
+    {
+        private string name;
+        private int itemID, quantity;
+        private double price;
+
+        // default constructor
+        public ItemClass()
+        {
+
+        }
+
+        // constructor
+        public ItemClass(int id, string n, int q, double p)
+        {
+            this.itemID = id;
+            this.name = n;
+            this.quantity = q;
+            this.price = p;
+        }
+
+        // getter
+        public int getItemID() { return this.itemID; }
+        public int getQuantity() { return this.quantity; }
+
+        // setter
+        public void setQuantity(int q)
+        {
+            this.quantity = q;
+        } 
+
+        // expression-bodied method to display info
+        public string display() => $"{itemID} {name}, price: {price:c}, {quantity} on hand";
+
+        // method to find an item in the array
+        public static int findIt(ItemClass[] inventory, int itemID)
+        {
+            for (int i = 0; i < inventory.Length; i++)
+            {
+                if (inventory[i].getItemID() == itemID)
+                {
+                    WriteLine($"Found it! {inventory[i].display()}");
+                    return i;
+                }
+                else
+                {
+                    WriteLine("Invalid ID number.");
+                }
+            }
+            return -1;
+        }
+
+        // method to set the quantity for restock
+
+        // method to subtract from the available quantity for sale.
+        // needs to return a boolean that indicates if the sale was successful or not.
+        // Only change the quantity if there is enough on hand for the sale.
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Console_Project_Inventory_Array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            int itemID, quantity, soldQty;
+            double price;
+            string reply;
+
+            WriteLine("Chapter 10 Console Project: Inventory Array by Jared Tims");
+
+            ItemClass temp = new ItemClass();
+
+            // inventory array
+            int size = 1;
+            ItemClass[] inventory = new ItemClass[size];
+
+            for (int i = 0;  i < inventory.Length; i++)
+            {
+                WriteLine("\nAdd items to inventory");
+                Write("Enter name of new item: ");
+                name = ReadLine();
+                Write("Enter ID number of new item: ");
+                itemID = int.Parse(ReadLine());
+                Write("Enter price of this time: $");
+                price = double.Parse(ReadLine());
+                Write("Enter quantity for this time: ");
+                quantity = int.Parse(ReadLine());
+                inventory[i] = new ItemClass(itemID, name, quantity, price);
+            }
+
+            WriteLine("\nInventory");
+            foreach (ItemClass item in inventory)
+            {
+                WriteLine($"{item.display()}");
+            }
+
+            do
+            {
+                Write("\nWhat do you want to do: (s)Sell, (r)Restock, (p)Print, (x)Exit? ");
+                reply = ReadLine();
+                if (reply == "s")
+                {
+                    Write("Enter item ID: ");
+                    itemID = int.Parse(ReadLine());
+                    //int spot = ItemClass.findIt(inventory, itemID);
+                    ItemClass.findIt(inventory, itemID);
+                    Write("How many sold? ");
+                    soldQty = int.Parse(ReadLine());
+                    if (soldQty < temp.getQuantity())
+                    {
+                        temp.quantity -= soldQty;
+                    }
+                    else
+                    {
+                        WriteLine("Not enough on hand to sell that many.");
+                    }
+                }
+                else if (reply == "r")
+                {
+
+                }
+                else if (reply == "p")
+                {
+
+                }
+                else if (reply == "x")
+                {
+
+                }
+                else
+                {
+                    WriteLine("Invalid option, please try again");
+                }
+            } while (reply != "x");
+
+            WriteLine("\nPress any key to continue...");
+            ReadKey();
+        }
+    }
+}
